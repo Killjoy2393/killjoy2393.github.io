8596cd292ad8013c9a91306c9eccbffc6b47375d
Killjoy2393 <42184284+Killjoy2393@users.noreply.github.com>
2019-02-14 19:22:06 +0200

    Production block
    add production block

---
+++ Biolex/js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+  $(window).on('load', function () {
+    var preloader = $('.preloader');
+    preloader.delay(3000).fadeOut(100);
+  });
+
+  $('.header-menu__hamburger').click(function () {
+    $('.header').animate({
+      height: '100%'
+    }, 500);
+    $(this).toggleClass('active');
+    $('.production-wrap').addClass('active');
+  });
+
+  $('.close-head').on('click', function() {
+    $('.header-menu__hamburger').removeClass('active');
+    $('.header').animate({
+      height: '0'
+    }, 500);
+    $('.production-wrap').removeClass('active');
+  });
+
+  $('.open').on('click', shForm);
+
+  function shForm() {
+    $(this).toggleClass('active');
+    $('.main-form').toggleClass('active');
+  };
+
+  $('.close').on('click', hForm)
+
+  function hForm() {
+    $('.main-form').removeClass('active');
+    $('.open').removeClass('active');
+  }
+
+  $('.owl-carousel').owlCarousel({
+    center: true,
+    items: 1,
+    dots: false, 
+    navContainer: '.production-nav'
+  });
+
+});
