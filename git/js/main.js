c72c627c528edcfe5e235d7ff3af54c919e12c99
Killjoy2393 <42184284+Killjoy2393@users.noreply.github.com>
2024-06-24 07:56:25 -0700

    add sheet
    add sheet

--- git/js/main.js
+++ git/js/main.js
@@ -1,45 +1,62 @@
 document.addEventListener("DOMContentLoaded", function() {
-  let formMain = document.getElementById("contactForm");
-  formMain.addEventListener('submit', async function(event) {
-    event.preventDefault();
-    const name = document.getElementById('nameInput').value.trim();
-    const phone = document.getElementById('phoneInput').value.trim();
-    const comment = document.getElementById('commentInput').value.trim();
-    console.log(name);
+    let countryCallingCode = ''
 
-    if (!name || !phone) {
-        alert('Пожалуйста, заполните обязательные поля.');
-        return;
+    let formMain = document.getElementById("contactForm");
+    formMain.addEventListener('submit', async function(event) {
+      event.preventDefault();
+      const name = document.getElementById('nameInput').value.trim();
+      console.log(name)
+      const phone = document.getElementById('phoneInput').value.trim();
+      const comment = document.getElementById('commentInput').value.trim();
+  
+      if (!name || !phone) {
+          alert('Пожалуйста, заполните обязательные поля.');
+          return;
+      }
+  
+      const data = {
+          name: name,
+          phone: countryCallingCode + phone,
+          comment: comment
+      };
+
+        console.log(data.phone)
+  
+      try {
+          const response = await fetch('https://script.google.com/macros/s/AKfycbxlZJz52sz9Pvgg5D9ZAzAGQZp6gYe9CoNi8eE1I7CNw8TK-2MA5LXyjX1Mxa4ZnnUfkA/exec', {
+              method: 'POST',
+              mode: 'no-cors',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(data)
+          });
+  
+          if (response) {
+              alert('Данные успешно отправлены!');
+              form.classList.remove('show')
+          } else {
+              alert('Произошла ошибка при отправке данных.');
+          }
+      } catch (error) {
+          console.error('Ошибка:', error);
+          alert('Произошла ошибка при отправке данных.');
+      }
+  
+      document.getElementById('contactForm').reset();
+    });
+
+
+  function validateNumericInput(event) {
+    const input = event.target;
+    const inputValue = input.value;
+    const numericValue = inputValue.replace(/\D/g, ''); // оставляем только цифры
+
+    if (inputValue !== numericValue) {
+        input.value = numericValue; // обновляем значение в поле
     }
-
-    const data = {
-        name: name,
-        phone: phone,
-        comment: comment
-    };
-
-    try {
-        const response = await fetch('https://script.google.com/macros/s/AKfycbxlZJz52sz9Pvgg5D9ZAzAGQZp6gYe9CoNi8eE1I7CNw8TK-2MA5LXyjX1Mxa4ZnnUfkA/exec', {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-
-        if (response.ok) {
-            alert('Данные успешно отправлены!');
-        } else {
-            alert('Произошла ошибка при отправке данных.');
-        }
-    } catch (error) {
-        console.error('Ошибка:', error);
-        alert('Произошла ошибка при отправке данных.');
-    }
-
-    document.getElementById('contactForm').reset();
-  });
+}
+document.getElementById("phoneInput").addEventListener('input', validateNumericInput)
 
   setPhoneInputMask();
 
@@ -60,8 +77,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
   (async () => {
     const countryCode = await getUserCountryCode();
+    countryCallingCode = await getCallingCode();
     if (countryCode) {
-        console.log(`Код страны: ${countryCode}`); // Код страны как строка
+        console.log(`Код страны: ${countryCode}`); 
+        
     } else {
         console.log('Не удалось получить код страны');
     }
@@ -74,11 +93,26 @@ document.addEventListener("DOMContentLoaded", function() {
             throw new Error('Не удалось получить данные о местоположении пользователя');
         }
         const data = await response.json();
+        countryCallingCode = data.country_calling_code
         return data.country_code.toLowerCase(); // Возвращает код страны в нижнем регистре (например, "ru" для России)
     } catch (error) {
         console.error('Ошибка при получении данных о местоположении:', error);
         return null;
     }
+  }
+
+  async function getCallingCode() {
+    try {
+        const response = await fetch('https://ipapi.co/json/');
+        if (!response.ok) {
+            throw new Error('Не удалось получить данные о местоположении пользователя');
+        }
+        const data = await response.json();
+        return data.country_calling_code.toLowerCase(); // Возвращает код страны в нижнем регистре (например, "ru" для России)
+    } catch (error) {
+        console.error('Ошибка при получении данных о местоположении:', error);
+        return null;
+    }    
   }
 
   async function setPhoneInputMask() {
